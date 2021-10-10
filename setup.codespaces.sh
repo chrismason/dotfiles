613b841823f6665d2f7f4bd62c1a17932535307c
Chris Mason <chrismason@github.com>
2021-10-10 16:23:55 -0400

    Codespaces debugging

--- setup.codespaces.sh
+++ setup.codespaces.sh
@@ -4,24 +4,25 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-# chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 
 rm -f $HOME/.zshrc
 
-apt-get update
-apt-get install -y \
-    ripgrep fzf fontconfig fuse
+sudo apt-get update
+sudo apt-get install -y \
+    ripgrep fzf fontconfig fuse python-pip3 neovim
 
 # Install fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d ~/.local/share/fonts
+mkdir -p $HOME/.local/share/fonts
+unzip FiraCode.zip -d ~HOME/.local/share/fonts
 fc-cache -fv
 rm FiraCode.zip
 
 # Install NeoVim Stable
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-mv nvim.appimage /usr/local/bin/nvim
+# wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+# chmod u+x nvim.appimage
+# sudo mv nvim.appimage /usr/local/bin/nvim
 
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/aliases.zsh $HOME/.aliases.zsh
