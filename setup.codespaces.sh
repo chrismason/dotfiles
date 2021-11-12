50d957edd704b7df5a5aaaaf7483f2d2b40a0d38
Chris Mason <6392972+chrismason@users.noreply.github.com>
2021-11-12 17:01:54 -0500

    Update setup.codespaces.sh

--- setup.codespaces.sh
+++ setup.codespaces.sh
@@ -4,14 +4,22 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+TMP_NVIM_DIR="/tmp/$USER/neovim"; mkdir -p $TMP_NVIM_DIR
+NVIM_DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
+
 sudo chsh -s "$(which zsh)" "$(whoami)"
 
 rm -f $HOME/.zshrc
 
-# sudo apt-add-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y \
-    ripgrep fzf fontconfig python3-pip # neovim
+    ripgrep fzf fontconfig python3-pip
+    
+wget $NVIM_DOWNLOAD_URL -P $TMP_NVIM_DIR
+tar -xzf "$TMP_NVIM_DIR/nvim-linux64.tar.gz"
+cp -RT "$TMP_NVIM_DIR/nvim-linux64/" "$HOME/.local" >/dev/null
+
+rm -rf $TMP_NVIM_DIR
 
 mkdir -p $HOME/.config
 
