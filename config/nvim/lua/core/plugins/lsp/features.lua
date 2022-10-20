e9e18b98c6449aed8d9f5035ac2b402de3517b64
Chris Mason <6392972+chrismason@users.noreply.github.com>
2022-10-20 11:48:02 -0400

    eslint

--- config/nvim/lua/core/plugins/lsp/features.lua
+++ config/nvim/lua/core/plugins/lsp/features.lua
@@ -1,6 +1,6 @@
 return {
 	prettier = vim.fn.executable("prettier") == 1,
-	eslint_d = vim.fn.executable("eslint_d") == 1,
+	eslint_d = vim.fn.executable("eslint") == 1,
 	golangci_lint = vim.fn.executable("golangci-lint") == 1,
 	rubocop = vim.fn.executable("bin/rubocop") == 1,
 	solargraph = vim.fn.executable("bin/solargraph") == 1,
