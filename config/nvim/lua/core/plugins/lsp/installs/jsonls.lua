b449125602652794b34ed74c9cc22b0214a6dd18
Chris Mason <6392972+chrismason@users.noreply.github.com>
2022-10-21 11:54:37 -0400

    schemas

--- config/nvim/lua/core/plugins/lsp/installs/jsonls.lua
+++ config/nvim/lua/core/plugins/lsp/installs/jsonls.lua
@@ -1,27 +1,35 @@
 local lspconfig = require("lspconfig")
 local config = require("core.plugins.lsp.defaults").defaults()
+local schemastore_ok, schemastore = pcall(require, "schemastore")
+
+local schemas
+if not schemastore_ok then
+    schemas = {
+        {
+            fileMatch = { "package.json" },
+            url = "https://json.schemastore.org/package.json"
+        },
+        {
+            fileMatch = { "tsconfig*.json" },
+            url = "https://json.schemastore.org/tsconfig.json"
+        },
+        {
+            fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
+            url = "https://json.schemastore.org/prettierrc.json"
+        },
+        {
+            fileMatch = { ".eslintrc", ".eslintrc.json" },
+            url = "https://json.schemastore.org/eslintrc.json"
+        },
+    }
+else
+    schemas = schemastore.json.schemas()
+end
 
 config.settings = {
-  json = {
-    schemas = {
-      {
-        fileMatch = { "package.json" },
-        url = "https://json.schemastore.org/package.json"
-      },
-      {
-        fileMatch = { "tsconfig*.json" },
-        url = "https://json.schemastore.org/tsconfig.json"
-      },
-      {
-        fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
-        url = "https://json.schemastore.org/prettierrc.json"
-      },
-      {
-        fileMatch = { ".eslintrc", ".eslintrc.json" },
-        url = "https://json.schemastore.org/eslintrc.json"
-      },
+    json = {
+        schemas = schemas
     }
-  }
 }
 
 lspconfig.jsonls.setup(config)
