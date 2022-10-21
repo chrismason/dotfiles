c1c277661b1fe6ff27110125b9429fae6e90bb94
Chris Mason <6392972+chrismason@users.noreply.github.com>
2022-10-21 08:48:22 -0400

    remove alias config

--- aliases.zsh
+++ aliases.zsh
@@ -5,8 +5,6 @@ alias t="tmux"
 alias ta="tmux attach -t"
 alias tn="tmux new-session -t"
 alias tl="tmux list-sessions"
-if command -v "bin/npm" &> /dev/null; then
-  alias npm="bin/npm"
-fi
+alias npm="bin/npm"
 export EDITOR=nvim
 export VISUAL=nvim
