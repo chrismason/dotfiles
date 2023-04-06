a6cd846cec9f79764dbc1264ea23dbd029867972
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-04-06 08:18:08 -0400

    Fix config

--- config/nvim/lua/plugins/lsp/config.lua
+++ config/nvim/lua/plugins/lsp/config.lua
@@ -6,7 +6,6 @@ vim.fn.sign_define("DiagnosticSignHint", { text = "" })
 
 -- Configure diagnostics displaying
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
     signs = true,
     update_in_insert = false,
     underline = true,
