8b7b4307b619c8d1bc0cb214f337e317d3badaca
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-04-18 21:58:35 -0400

    Tweaks

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -23,6 +23,8 @@ return {
             dapui.setup()
             dapgo.setup()
 
+            require('dap.ext.vscode').load_launchjs(nil, {})
+
             dap.listeners.after.event_initialized["dapui_config"] = function()
                 dapui.open(1)
             end
