f3677ed0d82bf2e7e11e052c7227ed11fa404ced
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-05-11 10:55:50 -0400

    Various updates

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -14,16 +14,16 @@ return {
             sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
         end,
         config = function()
-            local dap = require('dap')
-            local dapui = require('dapui')
-            local daptext = require('nvim-dap-virtual-text')
-            local dapgo = require('dap-go')
+            local dap = require("dap")
+            local dapui = require("dapui")
+            local daptext = require("nvim-dap-virtual-text")
+            local dapgo = require("dap-go")
 
             daptext.setup()
             dapui.setup()
             dapgo.setup()
 
-            require('dap.ext.vscode').load_launchjs(nil, {})
+            require("dap.ext.vscode").load_launchjs(nil, {})
 
             dap.listeners.after.event_initialized["dapui_config"] = function()
                 dapui.open(1)
@@ -38,21 +38,27 @@ return {
             end
         end,
         keys = {
-            { "<leader>vc", "<cmd>lua require'dap'.continue()<cr>" },
-            { "<leader>vs", function()
-                local dap = require('dap')
-                local dapui = require('dapui')
-                dap.close()
-                dapui.close()
-            end },
-            { "<leader>b",  "<cmd>lua require'dap'.toggle_breakpoint()<cr>" },
-            { "<leader>B", function()
-                local dap = require('dap')
-                dap.set_breakpoint(vim.fn.input('Breakpoint Condition: '))
-            end },
-            { "<leader>vv", "<cmd>lua require'dap'.step_over()<cr>" },
-            { "<leader>vi", "<cmd>lua require'dap'.step_into()<cr>" },
-            { "<leader>vo", "<cmd>lua require'dap'.step_out()<cr>" },
+            { "<F5>", "<cmd>lua require'dap'.continue()<cr>" },
+            {
+                "<S-F5>",
+                function()
+                    local dap = require("dap")
+                    local dapui = require("dapui")
+                    dap.close()
+                    dapui.close()
+                end,
+            },
+            { "<F9>", "<cmd>lua require'dap'.toggle_breakpoint()<cr>" },
+            {
+                "<S-F9>",
+                function()
+                    local dap = require("dap")
+                    dap.set_breakpoint(vim.fn.input("Breakpoint Condition: "))
+                end,
+            },
+            { "<F10>", "<cmd>lua require'dap'.step_over()<cr>" },
+            { "<F11>", "<cmd>lua require'dap'.step_into()<cr>" },
+            { "<F12>", "<cmd>lua require'dap'.step_out()<cr>" },
         },
     },
 }
--- config/nvim/lua/plugins/lsp/config.lua
+++ config/nvim/lua/plugins/lsp/config.lua
@@ -1,82 +1,82 @@
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "rounded",
+    border = "rounded",
 })
 
 local icons = require("plugins.lsp.icons")
 icons.setup_lsp_icons()
 icons.set_sign_icons({
-	error = "✘",
-	warn = "▲",
-	hint = "⚑",
-	info = "»",
+    error = "✘",
+    warn = "▲",
+    hint = "⚑",
+    info = "»",
 })
 
 local M = {}
 
 local function on_attach(client, bufnr)
-	local opts = { noremap = true, silent = true }
-	vim.keymap.set("n", "<leader>gd", '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>', opts)
-	vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-	vim.keymap.set("n", "<leader>gi", '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>', opts)
-	vim.keymap.set("n", "<leader>gt", '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>', opts)
-	vim.keymap.set("n", "<leader>gr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
-	vim.keymap.set("n", "<leader>gn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>gd", '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>', opts)
+    vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+    vim.keymap.set("n", "<leader>gi", '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>', opts)
+    vim.keymap.set("n", "<leader>gt", '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>', opts)
+    vim.keymap.set("n", "<leader>gr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
+    vim.keymap.set("n", "<leader>gn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 
-	-- diagnostics
-	vim.keymap.set("n", "[g", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
-	vim.keymap.set("n", "]g", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
-	vim.keymap.set("n", "<leader>ge", '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", })<cr>', opts)
-	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-	vim.keymap.set("n", "<leader>ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	vim.keymap.set("v", "<leader>ga", "<cmd>lua vim.lsp.buf.range_code_actions()<cr>", opts)
-	vim.keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
-	vim.keymap.set("n", "<C-K>", '<cmd>lua require("lsp_signature").signature()<cr>', opts)
+    -- diagnostics
+    vim.keymap.set("n", "[g", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+    vim.keymap.set("n", "]g", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+    vim.keymap.set("n", "<leader>ge", '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", })<cr>', opts)
+    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    vim.keymap.set("n", "<leader>ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    vim.keymap.set("v", "<leader>ga", "<cmd>lua vim.lsp.buf.range_code_actions()<cr>", opts)
+    vim.keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
+    vim.keymap.set("n", "<C-K>", '<cmd>lua require("lsp_signature").signature()<cr>', opts)
 
-	require("lsp_signature").on_attach({
-		bind = true, -- This is mandatory, otherwise border config won't get registered.
-		handler_opts = {
-			border = "single",
-		},
-	}, bufnr)
+    require("lsp_signature").on_attach({
+        bind = true, -- This is mandatory, otherwise border config won't get registered.
+        handler_opts = {
+            border = "single",
+        },
+    }, bufnr)
 
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
+    if client.server_capabilities.documentSymbolProvider then
+        require("nvim-navic").attach(client, bufnr)
+    end
 
-	if client.name == "omnisharp" then
-		local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
-		for i, v in ipairs(tokenModifiers) do
-			tokenModifiers[i] = v:gsub(" ", "_")
-		end
-		local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
-		for i, v in ipairs(tokenTypes) do
-			tokenTypes[i] = v:gsub(" ", "_")
-		end
-	end
+    if client.name == "omnisharp" then
+        local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+        for i, v in ipairs(tokenModifiers) do
+            tokenModifiers[i] = v:gsub(" ", "_")
+        end
+        local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+        for i, v in ipairs(tokenTypes) do
+            tokenTypes[i] = v:gsub(" ", "_")
+        end
+    end
 
-	print("LSP attached")
+    print("LSP attached")
 end
 
 require("fidget").setup({
-	text = {
-		spinner = "moon",
-	},
-	align = {
-		bottom = true,
-	},
-	window = {
-		relative = "editor",
-	},
+    text = {
+        spinner = "moon",
+    },
+    align = {
+        bottom = true,
+    },
+    window = {
+        relative = "editor",
+    },
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 M.defaults = function()
-	return {
-		capabilities = capabilities,
-		flags = { debounce_text_changes = 150 },
-		on_attach = on_attach,
-	}
+    return {
+        capabilities = capabilities,
+        flags = { debounce_text_changes = 150 },
+        on_attach = on_attach,
+    }
 end
 
 M.on_attach = on_attach
