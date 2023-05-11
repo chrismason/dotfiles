db6219d9e05f6953532babb4f72b7ceb202c7356
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-05-11 10:58:57 -0400

    Various updates (#12)

--- setup.osx.sh
+++ setup.osx.sh
@@ -9,3 +9,9 @@ ln -s $(pwd)/gitconfig $HOME/.gitconfig
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/config/nvim $HOME/.config/nvim
 ln -s $(pwd)/config/alacritty.yml $HOME/.config/alacritty.yml
+
+# Update ncurses
+# https://gist.github.com/bbqtd/a4ac060d6f6b9ea6fe3aabe735aa9d95
+curl -LO https://invisible-island.net/datafiles/current/terminfo.src.gz && gunzip terminfo.src.gz
+/usr/bin/tic -xe alacritty-direct,tmux-256color terminfo.src
+rm terminfo.src
