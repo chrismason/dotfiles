d614ed72e08d5583f6d3b531ddb759dd1d6323b7
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-05-31 07:37:17 -0400

    capabilities

--- config/nvim/lua/plugins/lsp/config.lua
+++ config/nvim/lua/plugins/lsp/config.lua
@@ -69,7 +69,10 @@ require("fidget").setup({
     },
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
+
+vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 M.defaults = function()
     return {
