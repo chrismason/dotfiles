f968f03e66a6d7008c86148a31a2409b54e8a8ee
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-06-15 14:21:54 -0400

    mappings

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -4,11 +4,12 @@ return {
         { "<leader>a",  "<cmd>lua require'harpoon.mark'.add_file()<cr>" },
         { "<leader>e",  "<cmd>lua require'harpoon.ui'.toggle_quick_menu()<cr>" },
         { "<leader>tc", "<cmd>lua require'harpoon.cmd-ui'.toggle_quick_menu()<cr>" },
-        { "<leader>h",  "<cmd>lua require'harpoon.ui'.nav_file(1)<cr>" },
-        { "<leader>j",  "<cmd>lua require'harpoon.ui'.nav_file(2)<cr>" },
-        { "<leader>k",  "<cmd>lua require'harpoon.ui'.nav_file(3)<cr>" },
-        { "<leader>l",  "<cmd>lua require'harpoon.ui'.nav_file(4)<cr>" },
+        { "<leader>1",  "<cmd>lua require'harpoon.ui'.nav_file(1)<cr>" },
+        { "<leader>2",  "<cmd>lua require'harpoon.ui'.nav_file(2)<cr>" },
+        { "<leader>3",  "<cmd>lua require'harpoon.ui'.nav_file(3)<cr>" },
+        { "<leader>4",  "<cmd>lua require'harpoon.ui'.nav_file(4)<cr>" },
+        { "<leader>5",  "<cmd>lua require'harpoon.ui'.nav_file(5)<cr>" },
         { "<leader>th", "<cmd>lua require'harpoon.term'.gotoTerminal(1)<cr>" },
         { "<leader>tj", "<cmd>lua require'harpoon.term'.gotoTerminal(2)<cr>" },
-    }
+    },
 }
