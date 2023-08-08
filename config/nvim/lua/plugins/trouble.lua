e3cc6148fcfaa797866acefcdc2f9ad317d36958
Chris Mason <6392972+chrismason@users.noreply.github.com>
2023-08-08 16:39:59 -0400

    Updates

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -1,21 +1,22 @@
 return {
-  "folke/trouble.nvim",
-  cmd = { "Trouble", "TroubleToggle" },
-  config = function()
-    require("trouble").setup({
-      auto_open = false,
-      auto_preview = true,
-      auto_jump = { "loclist", "quickfix", "lsp_definitions", "lsp_references", "lsp_type_definitions" },
-      padding = false,
-      use_diagnostic_signs = true,
-      action_keys = {
-        jump = { "<CR>" },
-        jump_close = { "o" },
-      },
-    })
-  end,
-  keys = {
-    { "<leader>wd", "<CMD>Trouble workspace_diagnostics<CR>", desc = "Workspace Diagnostics" },
-    -- { "<leader>dd", "<CMD>Trouble document_diagnostics<CR>",  desc = "Document Diagnostics" },
-  },
+	"folke/trouble.nvim",
+	cmd = { "Trouble", "TroubleToggle" },
+	config = function()
+		require("trouble").setup({
+			auto_open = false,
+			auto_preview = true,
+			auto_jump = { "loclist", "quickfix", "lsp_definitions", "lsp_references", "lsp_type_definitions" },
+			padding = false,
+			use_diagnostic_signs = true,
+			action_keys = {
+				jump = { "<CR>" },
+				jump_close = { "o" },
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>wd", "<CMD>Trouble workspace_diagnostics<CR>", desc = "Workspace Diagnostics" },
+		{ "<leader>tt", "<CMD>TroubleToggle<CR>", desc = "Workspace Diagnostics" },
+		-- { "<leader>dd", "<CMD>Trouble document_diagnostics<CR>",  desc = "Document Diagnostics" },
+	},
 }
