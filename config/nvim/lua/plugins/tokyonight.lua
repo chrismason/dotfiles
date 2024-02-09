b3d7ed23638f2ed8243dc0af1f9919173777f27e
Chris Mason <6392972+chrismason@users.noreply.github.com>
2024-02-09 08:55:17 -0500

    updates

--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -6,7 +6,45 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			style = "night",
+			transparent = true,
+			styles = {
+				sidebars = "dark",
+				floats = "dark",
+			},
+			on_colors = function(colors)
+				colors.bg_sidebar = "#011826"
+			end,
 			on_highlights = function(hl, c)
+				local prompt = "#0B2534"
+				local bgColor = "#011826"
+				hl.ToggleTerm = {
+					bg = bgColor,
+				}
+				hl.TelescopeNormal = {
+					bg = bgColor,
+					fg = c.fg_dark,
+				}
+				hl.TelescopeBorder = {
+					bg = bgColor,
+					fg = bgColor,
+				}
+				hl.TelescopePromptNormal = {
+					bg = prompt,
+				}
+				hl.TelescopePromptBorder = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePromptTitle = {
+					bg = prompt,
+					fg = "#1aFFFF",
+				}
+				hl.TelescopePreviewTitle = {
+					fg = "#1aFFFF",
+				}
+				hl.TelescopeResultsTitle = {
+					fg = "#1aFFFF",
+				}
 				hl.DiagnosticUnnecessary = {
 					fg = c.comment,
 				}
