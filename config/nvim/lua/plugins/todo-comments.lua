10fd5c69b1faa268f0225e82ae563f56dcd5690a
Chris Mason <6392972+chrismason@users.noreply.github.com>
2024-02-22 15:28:44 -0500

    updates

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -1,4 +1,15 @@
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = { "TodoTrouble", "TodoTelescope" },
+	event = "VimEnter",
+	keys = {
+		{
+			"<leader>xt",
+			"<CMD>TodoTrouble<CR>",
+			desc = "Open todos in Trouble",
+			noremap = true,
+			silent = true,
+		},
+	},
 }
