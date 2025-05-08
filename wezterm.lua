7896786e39d88341275be507d30e5ababd207448
Chris Mason <6392972+chrismason@users.noreply.github.com>
2025-05-08 09:21:56 -0400

    Wezterm updates

--- wezterm.lua
+++ wezterm.lua
@@ -74,6 +74,10 @@ config.keys = {
 		mods = "CTRL",
 		action = wezterm.action.CloseCurrentPane({ confirm = false }),
 	},
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 -- and finally, return the configuration to wezterm
